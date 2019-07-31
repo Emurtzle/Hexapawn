b32e3ea685114d5c406432bd9928c3129e98326d
Emily Wagner <emilywagner598@gmail.com>
2019-07-31 16:31:30 -0700

    Added some structure

---
+++ src/components/index/Index.js
@@ -0,0 +1,24 @@
+import React, { Component, Fragment } from 'react'
+
+class index extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+
+        }
+    }
+
+    render () {
+        // const {} = this.props
+        // const {} = this.state
+
+        return (
+            <Fragment>
+
+            </Fragment>
+        )
+    }
+}
+
+export default index
